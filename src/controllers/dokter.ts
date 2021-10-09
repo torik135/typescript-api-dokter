f0327a2d8b31ac89517136faa297dc2368be8a5b
torik <riqibrekele@gmail.com>
2021-10-09 21:11:47 +0700

    add get dokter

--- src/controllers/dokter.ts
+++ src/controllers/dokter.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import logging from '../config/logging'
 import { Connect, Query } from '../config/mysql'
+import IDokter from '../interfaces/dokter'
 
 const NAMESPACE = "DOKTER - CONTROLLER"
 
@@ -42,48 +43,46 @@ const getAllDokter = (req: Request, res: Response, next: NextFunction) => {
 
 }
 
-// const getDokter = (id: string, req: Request, res: Response, next: NextFunction) => {
-// 	logging.info(NAMESPACE, 'GET ALL DOKTER CONTROLLER')
+const getDokter = (req: Request, res: Response, next: NextFunction) => {
+	logging.info(NAMESPACE, 'GET ALL DOKTER CONTROLLER')
 
-// 	// let id = req.body.id
+	// let id = req.body.id
 
-// 	// console.log(id)
-// 	// console.log(typeof (id))
+	// console.log(id)
 
-// 	let query = `SELECT * FROM tb_dokter WHERE id = ${id}`
+	let { id, nama, spesialis } = req.body
+	// let num_id: number = id
+	console.log(typeof (id))
 
+	let query = `SELECT nama, spesialis from tb_dokter WHERE id = '${id}'`
 
-// 	Connect()
-// 		.then((connection) => {
-// 			Query(connection, query)
-// 				.then((results) => {
-// 					return res.status(200).json({
-// 						results
-// 					})
-// 				})
-// 				.catch((error) => {
-// 					logging.error(NAMESPACE, error.message, error)
+	Connect()
+		.then(connection => {
+			Query<IDokter[]>(connection, query)
+				.then((dokter) => {
+					return res.status(200).json({
+						dokter: dokter[0],
+					})
+				})
+				.catch(error => {
+					logging.error(NAMESPACE, error.message, error)
 
-// 					return res.status(500).json({
-// 						message: error.message,
-// 						error
-// 					})
-// 				})
-// 				.finally(() => {
-// 					connection.end()
-// 				})
-// 		})
-// 		.catch((error) => {
-// 			logging.error(NAMESPACE, error.message, error)
+					return res.status(500).json({
+						message: error.message,
+						error
+					})
+				})
+		})
+		.catch(error => {
+			logging.error(NAMESPACE, error.message, error)
 
-// 			return res.status(500).json({
-// 				message: error.message,
-// 				error
-// 			})
-// 		})
-// 
-// 
-// }
+			return res.status(500).json({
+				message: error.message,
+				error
+			})
+		})
+
+}
 
 const createDokter = (req: Request, res: Response, next: NextFunction) => {
 	logging.info(NAMESPACE, 'CREATE DOKTER CONTROLLER')
@@ -125,6 +124,6 @@ const createDokter = (req: Request, res: Response, next: NextFunction) => {
 
 export default {
 	getAllDokter,
-	// getDokter, 
+	getDokter,
 	createDokter
 }
